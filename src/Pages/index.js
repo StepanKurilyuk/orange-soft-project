6ab261c73a6fb8e1648be852b18715d5567a0ac0
Stepan Kurilyuk <rumml.nk@mail.ru>
2020-04-17 23:03:17 +0300

    Refactored pages import

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export {default as HomePage} from './HomePage/HomePage'
+export { NewsPage } from './NewsPage/NewsPage'
+export { LoginPage } from './LoginPage/LoginPage'
+export {default as ProfilePage} from './ProfilePage/ProfilePage'
