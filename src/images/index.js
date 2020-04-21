15f707618389dc3487d1deea5ab7657cd1ff54fb
Rumml <rumml.nk@mail.ru>
2020-04-21 15:31:39 +0300

    Added index.js for import logos on profile page

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+export {default as vkLogo} from './vk-logo.svg';
+export {default as linkedInLogo} from './linked-in-logo.svg';
+export {default as faceBookLogo} from './facebook-logo.svg';
+export {default as mapIcon} from './map-icon.svg';
+export {default as phoneIcon} from './phone-icon.svg';
+export {default as emailIcon} from './email-icon.svg';
+export {default as birthdayIcon} from './birthday-icon.svg';
