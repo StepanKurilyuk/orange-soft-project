bff00e6a9ed0685e9b599db13acf8dcd7e8963d7
Rumml <rumml.nk@mail.ru>
2020-04-21 15:29:23 +0300

    Updated private router for multi-use

--- src/Components/PrivateRouter/PrivateRouter.jsx
+++ src/Components/PrivateRouter/PrivateRouter.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
+export const PrivateRoute = ({ component: Component, redirectIf, redirectTo, ...rest}) => (
     <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        redirectIf
+            ? <Redirect to={{ pathname: redirectTo, state: { from: props.location } }} />
+            : <Component {...props} />
     )} />
 )
