0aa35e38232acefe1f174669f97df59fbe79c483
Rumml <rumml.nk@mail.ru>
2020-04-21 15:32:57 +0300

    Added Home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { NavigationBar } from '../../Components/NavigationBar/NavigationBar'
+import { NavigationBar } from '../../components/NavigationBar/NavigationBar'
+import './HomePage.css';
+import gitHubLogo from '../../images/github-icon.svg';
 
 export default function HomePage() {
     return (
         <>
             <NavigationBar selectedTabName='Home'/>
-            <div>
-                Hello Home Page
+            <div className='home-page-container' >
+                <h1>
+                    Test React Project
+                </h1>
+
+                <div className='home-page-connections'>
+                    <img src={ gitHubLogo } onClick={() => handleGitHubRedirect()} alt='git'/>
+                </div>
+
+                <div className='home-page-todos'>
+                    <h2>
+                        TODOS
+                    </h2>
+                    <ul>
+                        <li>
+                            Styles Reafactoring
+                        </li>
+
+                        <li>
+                            Add Pagintation with dots for news page
+                        </li>
+
+                        <li>
+                            Add Spinner
+                        </li>
+
+                        <li>
+                            Fix pagination style on mobile devices
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     )
+}
+
+function handleGitHubRedirect() {
+    window.location = 'https://github.com/Rumml/orange-soft-project';
 }
